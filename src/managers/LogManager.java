af8196ae441eb1c7bdd63d6ff296023660cfb2cb
Antonio Villasenor <carlosantovillaga@gmail.com>
2026-06-01 02:09:56 -0600

    se agrego LogManager.java

--- src/managers/LogManager.java
+++ src/managers/LogManager.java
@@ -1,5 +1,49 @@
 package managers;
 
-public class LogManager {
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class LogManager {
+	private static final String LOG_CAMBIOS = "cambiosproductos.log";
+	private static final String LOG_RESTOCK = "restock.log";
+	
+	private static String obtenerFechaHora() {
+		LocalDateTime ahora = LocalDateTime.now();
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return ahora.format(formato);
+	}
+	
+	//registra cuando se cambia un atributo de algun producto
+	//registra quien lo cambio , el id del producto, el campo que modifico, lo que habia antes y lo que quedo despues 
+	public static void registrarCambioProducto(String nombreEmpleado, String idProducto, String campoModificado, String antes, String despues) {
+		String mensaje = obtenerFechaHora() + " | Empleado: " + nombreEmpleado + " | ProID: " + idProducto + " | Modificado [" + campoModificado + "] de '" + antes + "' a '" + despues + "'";
+		escribirLinea(LOG_CAMBIOS, mensaje);
+	}
+	
+	//registra cuando se hace restock
+	public static void registrarRestock(String nombreEmpleado, String idProducto, int cantidadAgregada, int nuevoStockTotal) {
+		String mensaje = obtenerFechaHora() + " | Empleado: " + nombreEmpleado + " | ProdID" + idProducto + " | Agrego +" + cantidadAgregada + "unidades (Stock final: " + nuevoStockTotal + ")";
+		escribirLinea(LOG_RESTOCK, mensaje);
+	}
+	
+	//reutilizamos esta funcion para no escribirla muchas veces
+	private static void escribirLinea(String nombreArchivo, String lineaTexto) {
+		try {
+			//true para que añada la linea al final, que no sobreescriba
+			FileWriter archivo = new FileWriter(nombreArchivo, true);
+			PrintWriter escritor = new PrintWriter(archivo);
+			
+			//escribe la info
+			escritor.println(lineaTexto);
+			escritor.close();
+			archivo.close();
+			
+		} catch(Exception e) {
+			System.out.println("Error al guardar en ka bitacora (" + nombreArchivo + "):" + e.getMessage());
+			
+		}
+	}
+	
 }
